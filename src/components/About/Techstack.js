8df4bb2edcd76cc91a003362f6cdd058c3a4d16b
Fachruddin <poenyoex@gmail.com>
2024-12-11 18:30:35 +0700

    refactor Techstack component to remove unused tech icons for cleaner presentation

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -4,19 +4,16 @@ import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
   DiPython,
   DiGit,
   DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
   SiPhp,
   SiFirebase,
   SiLaravel,
   SiPostgresql,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
@@ -27,17 +24,12 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
+     
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiLaravel />
@@ -48,9 +40,7 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
       </Col>
